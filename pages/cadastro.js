b99eff82f2eb3a6ea0a54deb57a0fe12bf134500
Davi Ponce Manieri <dvponce3@gmail.com>
2024-01-26 10:21:25 -0300

    Paginas de Inscrição e login
    Aqui eu criei a página de cadastro e login, e linkei elas

---
+++ pages/cadastro.js
@@ -0,0 +1,2 @@
+import cadastro from "../src/screens/cadastro"
+export default cadastro
--- src/screens/cadastro/index.js
+++ src/screens/cadastro/index.js
@@ -12,15 +12,14 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
-
+import { revalidatePath } from 'next/cache';
 
 function Copyright(props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
       <Link color="inherit" href="https://mui.com/">
-        Manieri
+        Your Website
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -32,19 +31,7 @@ function Copyright(props) {
 
 const defaultTheme = createTheme();
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <Head>
-        <title>Seu Título Aqui</title>
-      </Head>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
-  );
-}
-
-export default function SignIn() {
+export default function SignUp() {
   const handleSubmit = (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -70,50 +57,73 @@ export default function SignIn() {
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
-            Login - NewTech System
+            Cadastro - NewTech System
           </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Nome usuário"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Senha"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
+          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
             <Button
               type="submit"
               fullWidth
               variant="contained"
-              sx={{ mt: 3, mb: 2,bgcolor: '#004AAD' }}
+              sx={{ mt: 3, mb: 2 }}
             >
-              Entrar
+              Sign Up
             </Button>
-            <Grid container justifyContent="center">
-                <Grid item>
-                  <Typography component="h1" variant="h5">
-                    <Link href="/cadastro" variant="body2">
-                      Não tem uma conta? Cadastrar.
-                    </Link>
-                  </Typography>
-                </Grid>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
               </Grid>
+            </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 8, mb: 4}} />
+        <Copyright sx={{ mt: 5 }} />
       </Container>
     </ThemeProvider>
   );
+  
 }
